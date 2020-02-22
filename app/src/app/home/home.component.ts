9ae9749f90e9d6e6ead29fa1d121d150f50a362d
Joshua Franklin <joshuafranklin26@gmail.com>
2020-02-22 16:39:10 -0700

    Finish iOS image processing

--- app/src/app/home/home.component.ts
+++ app/src/app/home/home.component.ts
@@ -5,28 +5,57 @@ import {ImageAsset} from 'tns-core-modules/image-asset/image-asset'
 import {Hold, HoldFinderService, Pixel, Image} from '~/app/holdfinder/holdfinder.service';
 import {isAndroid} from 'tns-core-modules/platform';
 
+declare var android: any;
+declare var PHImageManager: any;
+declare var PHImageRequestOptions: any;
+declare var PHImageRequestOptionsResizeMode: any;
+declare var PHImageRequestOptionsDeliveryModeHighQualityFormat: any;
+declare var PHImageContentModeAspectFill: any;
+declare var CGDataProviderCopyData: any;
+declare var CGImageGetDataProvider: any;
+declare var CFDataGetBytePtr: any;
+
+type UIImage = any;
+
+const SCALE_DOWN_FACTOR = 50;
+
 @Component({
     selector: 'Home',
     templateUrl: './home.component.html'
 })
 export class HomeComponent {
-
     constructor(
         private holdFinderService: HoldFinderService,
     ) {
+        this.processImageIOS = this.processImageIOS.bind(this);
+        this.getUIImagePixel = this.getUIImagePixel.bind(this);
     }
 
-    processImageAndroid(imageAsset: ImageAsset): Image {
-        // Type checking is ignored since `android` is built-in.
-        // @ts-ignore
+    private iOSImageData: any = null;
+
+    private getUIImagePixel(image: UIImage, x: number, y: number): Pixel {
+        if (this.iOSImageData == null) {
+            this.iOSImageData = CFDataGetBytePtr(CGDataProviderCopyData(CGImageGetDataProvider(image.CGImage)));
+        }
+
+        const pixelInfo = ((image.size.width  * x) + y) * 4;
+
+        return new Pixel(
+            this.iOSImageData[pixelInfo],
+            this.iOSImageData[pixelInfo + 1],
+            this.iOSImageData[pixelInfo + 2],
+        );
+    }
+
+    private async processImageAndroid(imageAsset: ImageAsset): Promise<Image> {
         const bitmap = android.graphics.BitmapFactory.decodeFile(imageAsset.android, null);
         const width = bitmap.getWidth();
         const height = bitmap.getHeight();
 
         const image: Pixel[][] = [];
-        for (let x = 0; x < width; ++x) {
+        for (let x = 0; x < width; x += SCALE_DOWN_FACTOR) {
             const row: Pixel[] = [];
-            for (let y = 0; y < height; ++y) {
+            for (let y = 0; y < height; y += SCALE_DOWN_FACTOR) {
                 row.push(bitmap.getColor(x, y));
             }
             image.push(row);
@@ -35,19 +64,38 @@ export class HomeComponent {
         return image;
     }
 
-    processImageIOS(imageAsset: ImageAsset): Image {
-        // TODO
-        return [];
+    private async processImageIOS(imageAsset: ImageAsset): Promise<Image> {
+        return new Promise<Image>((success, error) => {
+            let manager = PHImageManager.defaultManager();
+            let options = new PHImageRequestOptions();
+            options.resizeMode = PHImageRequestOptionsResizeMode.Exact;
+            options.deliveryMode = PHImageRequestOptionsDeliveryModeHighQualityFormat;
+
+            manager.requestImageForAssetTargetSizeContentModeOptionsResultHandler(
+                imageAsset.ios,
+                {width: 2048, height: 1536},
+                PHImageContentModeAspectFill,
+                options,
+                (result: UIImage, info) => {
+                    const image: Pixel[][] = [];
+                    for (let x = 0; x < result.size.width; x += SCALE_DOWN_FACTOR) {
+                        const row: Pixel[] = [];
+                        for (let y = 0; y < result.size.height; y += SCALE_DOWN_FACTOR) {
+                            row.push(this.getUIImagePixel(result, x, y));
+                        }
+                        image.push(row);
+                    }
+
+                    success(image);
+                },
+            );
+        });
     }
 
-    findHolds(imageAsset: ImageAsset): Hold[] {
+    async findHolds(imageAsset: ImageAsset): Promise<Hold[]> {
         let image: Image;
 
-        if (isAndroid) {
-            image = this.processImageAndroid(imageAsset);
-        } else {
-            image = this.processImageIOS(imageAsset);
-        }
+        image = await (isAndroid ? this.processImageAndroid : this.processImageIOS)(imageAsset);
 
         return this.holdFinderService.findHolds(image);
     }
@@ -55,8 +103,12 @@ export class HomeComponent {
     public takePicture() {
         camera.requestCameraPermissions().then(() => {
             camera.takePicture().then(imageAsset => {
+                // TODO: Move to loading page.
+
                 // TODO: Do something with this value.
-                this.findHolds(imageAsset);
+                this.findHolds(imageAsset).then(holds => {
+                    // TODO: Move to results page.
+                });
             }).catch(error => {
                 alert(error);
             })
@@ -64,5 +116,4 @@ export class HomeComponent {
             alert('Camera permission is required.');
         });
     }
-
 }
