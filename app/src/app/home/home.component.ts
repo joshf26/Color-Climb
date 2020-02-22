6146c5ffe29d4941ac682c485309769abbe7b048
TiffanyVPhan <tiffany.phan@colorado.edu>
2020-02-22 14:04:34 -0700

    added background img for landing page

--- app/src/app/home/home.component.ts
+++ app/src/app/home/home.component.ts
@@ -4,9 +4,13 @@ import { alert } from "tns-core-modules/ui/dialogs";
 
 @Component({
     selector: "Home",
-    templateUrl: "./home.component.html"
+    templateUrl: "./home.component.html",
+    styleUrls: ["./home.component.css"]
 })
+
 export class HomeComponent {
+
+    public img = '~/images/wall1.png';
 
     public takePicture() {
         camera.requestCameraPermissions().then(() => {
