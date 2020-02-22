117062426a33348430bd9e6672895fc6d76d6344
Joshua Franklin <joshuafranklin26@gmail.com>
2020-02-22 12:15:49 -0700

    Add take picture button

--- app/src/app/home/home.component.ts
+++ app/src/app/home/home.component.ts
@@ -1,7 +1,23 @@
 import { Component } from "@angular/core";
+import * as camera from "nativescript-camera";
+import { alert } from "tns-core-modules/ui/dialogs";
 
 @Component({
     selector: "Home",
     templateUrl: "./home.component.html"
 })
-export class HomeComponent { }
+export class HomeComponent {
+
+    public takePicture() {
+        camera.requestCameraPermissions().then(() => {
+            camera.takePicture().then(imageAsset => {
+                alert('Picutre taken successfully.');
+            }).catch(error => {
+                alert('Error taking picture.');
+            })
+        }).catch(error => {
+            alert('Camera permission is required.');
+        });
+    }
+
+}
