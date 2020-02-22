7fe31516d830f216a4115428781533ad13fafe07
Elizabeth Qiu <eqiu21@gmail.com>
2020-02-22 15:19:59 -0700

    separate test file

---
+++ colorNormalize.ts
@@ -0,0 +1,54 @@
+// tsc <filename.ts>
+// node <filename.js>
+
+export class Pixel {
+    constructor(
+        public r: number,
+        public g: number,
+        public b: number,
+    ) {}
+}
+
+export class Hold {
+    constructor(
+        public positionX: number,
+        public positionY: number,
+        public radius: number,
+        public routeId: number,
+    ) {}
+}
+
+export function findHolds(image: Pixel[][]): Hold[] {
+    const holds: Hold[] = [];
+
+    // Code goes here...
+
+    return holds;
+}
+interface ImageContext {
+    context: CanvasRenderingContext2D;
+    width: number;
+    height: number;
+}
+
+
+
+
+function test() {
+    const image = [
+        [new Pixel(0, 0, 0), new Pixel(0, 0, 0), new Pixel(0, 0, 0)],
+        [new Pixel(0, 0, 0), new Pixel(255, 0, 0), new Pixel(0, 0, 0)],
+        [new Pixel(0, 0, 0), new Pixel(0, 0, 0), new Pixel(0, 0, 0)],
+    ]
+
+    const holds = findHolds(image);
+    // r,g,b, 
+    if (holds[0] == new Hold(1, 1, 1, 0)) {
+        console.log('Good job guys');
+    } else {
+        console.log('WRONG');
+    }
+}
+
+
+test();
