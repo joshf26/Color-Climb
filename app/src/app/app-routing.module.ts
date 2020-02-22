3b8fccdb95e58a5098af470dfd06564f1c96efce
Joshua Franklin <joshuafranklin26@gmail.com>
2020-02-22 14:05:57 -0700

    Move holdfinder to service and add android image processing

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from "@angular/core";
-import { NativeScriptRouterModule } from "nativescript-angular/router";
-import { Routes } from "@angular/router";
+import {NgModule} from '@angular/core';
+import {NativeScriptRouterModule} from 'nativescript-angular/router';
+import {Routes} from '@angular/router';
 
-import { HomeComponent } from "./home/home.component";
+import {HomeComponent} from './home/home.component';
 
 const routes: Routes = [
-    { path: "", redirectTo: "/home", pathMatch: "full" },
-    { path: "home", component: HomeComponent },
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent},
 ];
 
 @NgModule({
