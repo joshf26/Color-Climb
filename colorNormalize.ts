7480f94f174718ecfcf553613677bc69d0bc6f3f
Elizabeth Qiu <eqiu21@gmail.com>
2020-02-22 16:18:22 -0700

    restructure to class

--- colorNormalize.ts
+++ colorNormalize.ts
@@ -1,3 +1,4 @@
+import * as Collections from 'typescript-collections';
 // tsc <filename.ts>
 // node <filename.js>
 
@@ -18,38 +19,59 @@ export class Hold {
     ) {}
 }
 
-export function findHolds(image: Pixel[][]): Hold[] {
-    const holds: Hold[] = [];
-    // parse through the image
-    const pixelcounter: Pixel[] = [];
-    for(var i = 0; i < image.length; i++) {
-        var pixel = image[i];
+export type Image = Pixel[][];
 
-        for(var j = 0; j < pixel.length; j++) {
-            console.log("cube[" + i + "][" + j + "] = " + pixel[j].r);
-            pixelcounter.push(pixel[j])
+export class HoldFinderService {
+    constructor() {  }
+
+    public findHolds(image: Image): Hold[] {
+        const holds: Hold[] = [];
+        // parse through the image
+        // const pixelcounter: Pixel[] = [];
+        // for(var i = 0; i < image.length; i++) {
+        //     var pixel = image[i];
+
+        //     for(var j = 0; j < pixel.length; j++) {
+        //         console.log("cube[" + i + "][" + j + "] = " + pixel[j].r);
+        //         pixelcounter.push(pixel[j])
+        //     }
+        // }
+        // console.log(pixelcounter)
+
+        var dict = new Collections.Dictionary<Pixel, number>();
+        for(var i = 0; i < image.length; i++) {
+            var pixel = image[i];
+
+            for(var j = 0; j < pixel.length; j++) {
+                console.log("cube[" + i + "][" + j + "] = " + pixel[j].r);
+                if (dict.containsKey(pixel[j])) {
+                    var inc = dict.getValue(pixel[j])
+                    dict.setValue(pixel[j],inc+1)
+                }
+                else {
+                    dict.setValue(pixel[j],1)
+                }
+                
+            }
         }
+        console.log(dict)
+
+        return holds;
     }
-    console.log(pixelcounter)
-    return holds;
-}
-interface ImageContext {
-    context: CanvasRenderingContext2D;
-    width: number;
-    height: number;
 }
 
 
 
 
 function test() {
+    const hfs:HoldFinderService = new HoldFinderService();
     const image = [
         [new Pixel(0, 0, 0), new Pixel(0, 0, 0), new Pixel(0, 0, 0)],
         [new Pixel(0, 0, 0), new Pixel(255, 0, 0), new Pixel(0, 0, 0)],
         [new Pixel(0, 0, 0), new Pixel(0, 0, 0), new Pixel(0, 0, 0)],
     ]
 
-    const holds = findHolds(image);
+    const holds = hfs.findHolds(image);
     // r,g,b, 
     if (holds[0] == new Hold(1, 1, 1, 0)) {
         console.log('Good job guys');
