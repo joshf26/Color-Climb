0900fc0ef3f88f842fd674e933febd29988870a7
Joshua Franklin <joshuafranklin26@gmail.com>
2020-02-23 09:46:51 -0700

    Add basic results screen and cleanup

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -3,13 +3,11 @@ import {NativeScriptRouterModule} from 'nativescript-angular/router';
 import {Routes} from '@angular/router';
 
 import {HomeComponent} from './home/home.component';
-import {LoadingComponent} from "~/app/loading/loading.component";
 import {ResultsComponent} from "~/app/results/results.component";
 
 const routes: Routes = [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
-    {path: 'loading', component: LoadingComponent},
     {path: 'results', component: ResultsComponent},
 ];
 
