6095f3180cbfcd7dba949be261d76f55ae5cd9eb
Joshua Franklin <joshuafranklin26@gmail.com>
2020-02-22 21:45:39 -0700

    josh new changes

--- app/src/app/results/results.component.ts
+++ app/src/app/results/results.component.ts
@@ -1,7 +1,17 @@
 import {Component} from '@angular/core';
+import {Hold} from "~/app/holdfinder/holdfinder.service";
 
 @Component({
     selector: "Results",
     templateUrl: "./results.component.html",
 })
-export class ResultsComponent {}
+export class ResultsComponent {
+    private holds: Hold[] = [];
+
+    public setResults(holds: Hold[]) {
+        console.log('############## Holds set.');
+        this.holds = holds;
+        console.log(`############# ${holds}`);
+    }
+
+}
